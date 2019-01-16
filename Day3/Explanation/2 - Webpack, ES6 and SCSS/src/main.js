8da57f52bca66b2e32923aa0fc01bc7d54e762ec
Jose Antonio Postigo <japostigo@atsistemas.com>
2019-01-16 16:01:31 +0100

    Added Day3 Explanation

---
+++ Day3/Explanation/2 - Webpack, ES6 and SCSS/src/main.js
@@ -0,0 +1,21 @@
+//1. Transpilación: http://babeljs.io/repl/
+
+//2. Declaración de clases (objetos)
+
+//3. Importación/exportación de módulos
+
+//4. const, var, let, windows.
+
+//5. Funciones flecha, contexto del this
+
+//6. Plantillas en cadenas (``), funcion includes
+
+//7. Valores por defecto en parámetros,
+
+//8. Objetos literales:
+//     - Simplificación propiedad-valor let a = {b, c: 'val'}
+//     - Simplificación declaración de función
+
+//9. Spread operator
+
+//10. Bucles for para iterar arrays y claves de objetos
